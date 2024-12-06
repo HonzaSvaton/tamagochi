4470285966a5cc1804810e261cc16ace3663673f
HonzaSvaton <jan.svaton@outlook.cz>
2024-12-06 09:30:33 +0100

    first commit

---
+++ tamagochi/Pet.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagochi
+{
+    public abstract class pet
+    {
+        // public int Hunger { get; private set; } = 50;
+        private int hunger = 50;
+        public int Hunger
+        {
+            get
+            {
+                return hunger;
+            }
+            set
+            {
+                hunger = value;
+                if (!IsAlive() )
+                {
+                    throw new DeadPetEcxeption();
+                }
+            }
+        }
+        private Random random = new Random();
+
+        public int Do()
+        {
+            var toss = random.Next(1,20);
+            if (toss < 10)
+            {
+                Chill();
+            }
+            else if (toss < 16)
+            {
+                Play();
+            }
+            else
+            {
+                Feed();
+            }
+            return toss;
+        }
+
+        public int Boredom { get; private set; } = 50;
+        public void Feed()
+        {
+            Hunger -= 15;
+            Boredom += 5;
+            Console.WriteLine("papá");
+        }
+        public void Play()
+        {
+            Hunger += 20;
+            Boredom -= 25;
+            Console.WriteLine("Hraje si");
+        }
+        public void Chill()
+        {
+            Hunger += 10;
+            Boredom += 10;
+            Console.WriteLine("Odpočívá");
+        }
+        public bool IsAlive()
+        {
+            if(Hunger>100 || Hunger<0)
+            {  
+                return false; 
+            }
+            return true;
+        }
+        public virtual string Whoami()
+        {
+            return "pet";
+        }
+    }
+}
---
+++ tamagochi/Program.cs
@@ -0,0 +1,44 @@
+﻿using tamagochi;
+try
+{
+    var panda = new Panda();
+    var who = panda.Whoami();
+    Console.WriteLine(who);
+    while (true)
+    {
+        var tosss = panda.Do();
+        Thread.Sleep(500);
+        Console.WriteLine($"toss was {tosss}");
+        if (tosss > 8)
+        {
+            Console.WriteLine("F = Nakrmit");
+            Console.WriteLine("P = Hrát si");
+            var action = Console.ReadLine();
+
+            while (action != "exit")
+            {
+                if (action == "F")
+                {
+                    panda.Feed();
+                    break;
+                }
+                if (action == "P")
+                {
+                    panda.Play();
+                    break;
+                }
+            }
+        }
+        Console.Write("Hlad: ");
+        Console.WriteLine(panda.Hunger);
+        Console.Write("Znuděnost: ");
+        Console.WriteLine(panda.Boredom);
+        Console.WriteLine(panda.IsAlive() ? "žije" : "nežije");
+        Console.WriteLine("\n");
+    }
+}
+catch (DeadPetEcxeption)
+{
+    Console.WriteLine("smůla Panda je mrtvá");
+}
+
